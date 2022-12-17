03eb7911172f34cc5f6bb4480c78ae209cc31c2e
Yaron M. Minsky <yminsky@gmail.com>
2022-12-17 10:43:33 -0500

    small cleanup

--- bench/bench.ml
+++ bench/bench.ml
@@ -38,6 +38,8 @@ module Parallel = struct
   module Task = Domainslib.Task
 
   let rec edit_distance pool s t =
+    let async f = Task.async pool f in
+    let await x = Task.await pool x in
     match String.length s, String.length t with
     | 0, x | x, 0 -> x
     | len_s, len_t ->
@@ -46,12 +48,11 @@ module Parallel = struct
       let cost_to_drop_both =
         if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
       in
-      let async f = Task.async pool f in
       let d1 = async (fun () -> edit_distance pool s' t + 1) in
       let d2 = async (fun () -> edit_distance pool s t' + 1) in
       let d3 = async (fun () -> edit_distance pool s' t' + cost_to_drop_both) in
       let ( ++ ) = Int.min in
-      Task.await pool d1 ++ Task.await pool d2 ++ Task.await pool d3
+      await d1 ++ await d2 ++ await d3
   ;;
 
   let edit_distance ~num_domains s t =
